e511c0f316fb5bed785d581c1716f2d79728c434
thenewstockton <phillip5914619@gmail.com>
2018-01-23 23:33:55 +0800

    Assertions

--- UnitTests/UnitTests/Class1.cs
+++ UnitTests/UnitTests/Class1.cs
@@ -10,9 +10,53 @@ namespace UnitTests
     [TestFixture]
     public class BankAccountTests
     {
+        private BankAccount ba;
+        [SetUp]
+        public void SetUp()
+        {
+            ba = new BankAccount(100);
+        }
+
         [Test]
         public void BankAccountShouldIncreaseOnPositiveDeposit()
         {
+            //Assert.That(2 + 2, Is.EqualTo(4));
+            //Assert.Fail("This must fail");
+            //Assert.Inconclusive("hello");
+            //Assert.Warn("This is not good");
+            //Warn.If(2 + 2 != 5);
+            //Warn.If(2 + 2, Is.Not.EqualTo(5));
+            //Warn.If(() => 2 + 2, Is.Not.EqualTo(5).After(2000));
+            //Warn.Unless(2 + 2 == 5);
+            //Warn.Unless(2 + 2, Is.EqualTo(5));
+            //Warn.Unless(() => 2 + 2, Is.EqualTo(5).After(2000));
+            
+            ba.Deposit(100);
+            //Assert.AreEqual(200, ba.Balance);
+            Assert.That(ba.Balance, Is.EqualTo(200));
+        }
+
+        [Test]
+        public void BankAccountShouldThrowOnNonPositiveAmount()
+        {
+            var ex = Assert.Throws<ArgumentException>(() =>
+            {
+                ba.Deposit(-1);
+            });
+
+            StringAssert.StartsWith("Deposit amount must be positive", ex.Message);
+        }
+
+        [Test]
+        public void MultipleAsserts()
+        {
+            ba.Withdraw(100);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(ba.Balance, Is.EqualTo(0));
+                Assert.That(ba.Balance, Is.LessThan(1));
+            });
             
         }
     }
@@ -28,12 +72,16 @@ namespace UnitTests
 
         public void Deposit(int amount)
         {
-
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Deposit amount must be positive", nameof(amount));
+            }
+            Balance += amount;
         }
 
         public void Withdraw(int amount)
         {
-
+            Balance -= amount;
         }
     }
 }
